a784e6134a28e942c227668f31494187699df6ee
allgjatap <allgjatapatricia@gmail.com>
2023-01-27 09:50:09 +0100

    first commit

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { BookmarkModel } from 'src/app/models/bookmark-model';
+import bookmarks from '../../bookmarks.json'
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.scss']
+  styleUrls: ['./bookmarks.component.scss'],
 })
 export class BookmarksComponent {
+  bookmarkModel: BookmarkModel[] = bookmarks;
+  
 
+  constructor() {}
+
+  ngOnInit() {}
 }
---
+++ src/app/models/bookmark-model.ts
@@ -0,0 +1,5 @@
+export interface BookmarkModel {
+    image: string;
+    title: string;
+    link: string;
+}
